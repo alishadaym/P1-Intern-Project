9d692059790a72b5b5c355979067beb8e75c3d82
alishadaym <alishadamiawork@gmail.com>
2026-08-26 16:33:42 +0800

    modified main route

--- app.py
+++ app.py
@@ -626,8 +626,8 @@ def scans():
 def shops():
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 # if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
