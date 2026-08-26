9904361ecd1960ba94f52a82dfba2e81359f572c
alishadaym <alishadamiawork@gmail.com>
2026-08-26 17:43:14 +0800

    fix

--- app.py
+++ app.py
@@ -117,6 +117,7 @@ def search_shops():
             s.shop_name,
             s.operating_hours,
             s.category,
+            s.unit,
             s.description,
             s.floor_id,
             f.floor_name,
