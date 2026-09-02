2c4a32b20ea71c3685898c29404d9dbbf35cec79
alishadaym <alishadamiawork@gmail.com>
2026-09-02 09:56:33 +0800

    fix

--- app.py
+++ app.py
@@ -747,5 +747,5 @@ def shops():
 def chat_page():
     return render_template("chat.html")
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
